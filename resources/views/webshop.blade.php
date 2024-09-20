9ea9e0345722d9174957e82a2104d539d88ade7a
Rayan Moumine <rayanmoumine11@gmail.com>
2024-09-20 11:40:23 +0200

    /push webshop

--- resources/views/webshop.blade.php
+++ resources/views/webshop.blade.php
@@ -1,30 +1,31 @@
-<x-layout>
-    <div class="background-container"></div>
-    <div class="main-content">
-        <section class="about">
-            <div class="container">
-                <div class="about">
-                    <h2>Webshop</h2>
-                </div>
-        </section>
-    </div>
-
-    <div class="container">
-        <div class="row">
-            @foreach($products as $product)
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $product->name }}</h5>
-                            <p class="card-text">{{ $product->description }}</p>
-                            <p class="card-text">Price: ${{ $product->price }}</p>
-                            <p class="card-text">In stock: {{ $product->stock }}</p>
-                        </div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        @foreach($products as $product)
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->name }}</h5>
+                        <p class="card-text">{{ $product->description }}</p>
+                        <p class="card-text">Price: ${{ $product->price }}</p>
+                        <p class="card-text">In stock: {{ $product->stock }}</p>
                     </div>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
+</div>
 
-</x-layout>
+</body>
+
+</html>
---
+++ resources/views/webshopp.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <div class="background-container"></div>
+    <div class="main-content">
+        <section class="about">
+            <div class="container">
+                <div class="about">
+                    <h2>Webshop</h2>
+                </div>
+        </section>
+    </div>
+
+
+
+</x-layout>
