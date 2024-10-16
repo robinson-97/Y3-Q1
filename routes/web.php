243be6d038f5de33992684a9ccc0914228ff5e01
robinson <robinson.lachaud@gmail.com>
2024-10-16 21:08:09 +0200

    Appointment pagina alleen nog ingelogd te bereiken

--- routes/web.php
+++ routes/web.php
@@ -1,67 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AfspraakController;
+use App\Http\Controllers\WebshopController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Homepagina
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
+// Contactpagina
 Route::get('/views/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
+// Nieuws pagina
 Route::get('/views/nieuws', function () {
     return view('nieuws');
-});
+})->name('nieuws');
 
+// FAQ pagina
 Route::get('/views/faq', function () {
     return view('faq');
-});
+})->name('faq');
 
+// About pagina
 Route::get('/views/about', function () {
     return view('about');
-});
+})->name('about');
 
-Route::get('/views/webshop', function () {
-    return view('about');
-});
+// Webshop pagina
+Route::get('/views/webshop', [WebshopController::class, 'index'])->name('webshop');
 
+// Aanvraag pagina
 Route::get('/views/aanvraag', function () {
     return view('aanvraag');
-});
+})->name('aanvraag');
 
-
-Route::get('/views/afspraak', function () {
-    return view('afspraak');
-});
-
+// Bedankt pagina
 Route::get('/views/bedankt', function () {
     return view('bedankt');
-});
+})->name('bedankt');
 
-Route::get('/bedankt', function () {
-    return view('bedankt');
-});
+// Afspraken pagina - Alleen toegankelijk voor ingelogde gebruikers
+Route::get('/views/afspraak', [AfspraakController::class, 'index'])->middleware('auth')->name('afspraak');
 
-
-
-
-
-Route::get('/afspraak', function () {
-    return view('afspraak');
-});
-
-
-Route::get('/afspraak', [AfspraakController::class, 'index'])->name('afspraak');
-
-use App\Http\Controllers\AfspraakController;
-
-Route::get('/afspraak', [AfspraakController::class, 'index'])->name('afspraak');
-
-
-
-Route::get('/webshop', [WebshopController::class, 'index']);
-
+// Authenticatie routes (Laravel standaard)
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Gebruikersdashboard/home na inloggen
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
